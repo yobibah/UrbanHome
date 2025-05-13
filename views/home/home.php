936abb423fffab75e1d39270bf3ad279e8c733c1
hp <mnacoulmaezekiel@gmail.com>
2025-05-13 14:18:03 +0000

    modified:   views/home/home.php

--- views/home/home.php
+++ views/home/home.php
@@ -1,8 +1,7 @@
-<?php
-require_once VIEW_PATH . 'layout/header.php';
-require_once VIEW_PATH . 'layout/footer.php';
-?>
+<?php require_once VIEW_PATH . 'layout/header.php'; ?>
+
 <div class="home">
     ici ces home
 </div>
 
+<?php require_once VIEW_PATH . 'layout/footer.php'; ?>
